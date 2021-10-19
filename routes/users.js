3602a22564eea1b206a9d9a6c452ee72d6805bbb
Mika Pennanen <mika.pennanen7@gmail.com>
2021-10-19 11:20:52 +0300

    testit tehty

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/eka', function(reguest,response ){
+  console.log("Ekaa kutsuttiin");
+  response.send('Eka vastaa');
+});
+
+router.use(function(request, response, next){
+  console.log("Olen Middleware funktio");
+  next();
+})
+
+router.get('/toka/:fname', function(request, response){
+  console.log("Etunimi= " + request.params.fname);
+  response.send("Terve " + request.params.fname);
+});
+
+router.post('/', function(request,response){
+  console.log(request.body);
+  response.send(request.body);
+});
+
+router.post('/kolmas', function(request,response){
+  console.log(request.body.firstname);
+  response.send(request.body.lastname);
+});
+
+router.put('/:id', function(request, response){
+  let id=request.params.id;
+  console.log("kirjan " + id + " kirjan uusi nimi: " + request.body.name);
+  response.send("kirjan " + id + " kirjan uusi nimi: " + request.body.name);
+});
+
 module.exports = router;
